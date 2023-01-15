4ffb9603e3df50a38bead7a47aa4f178ddbb2bba
jeffte costa <jefftewf17@outlook.com>
2023-01-15 13:35:45 -0300

    feat: add button element

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,28 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./style";
 
 export function Home() {
+	function handleParticipantAdd() {
+		console.log("Você clicou no botão de Adicionar");
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.eventName}>
-				Nome do evento
-			</Text>
+			<Text style={styles.eventName}>Nome do evento</Text>
 
-			<Text style={styles.eventDate}>
-				Domingo, 15 de janeiro de 2023.
-			</Text>
+			<Text style={styles.eventDate}>Domingo, 15 de janeiro de 2023.</Text>
 
-			<TextInput
-				style={styles.input}
-				placeholder="Nome do participante"
-				placeholderTextColor="#6b6b6b"
-			/>
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder="Nome do participante"
+					placeholderTextColor="#6b6b6b"
+				/>
+
+				<TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
